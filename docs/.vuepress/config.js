8a458ad4651a60e3966b8d306711be52e56e2644
Ffloriel <florielfedry@gmail.com>
2019-05-27 17:52:10 +0200

    add docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'Tosin',
+  description: '',
+  base: '/tosin/',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/Guide.md'},
+    ],
+    sidebar: 'auto',
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'FullHuman/tosin',
+    // if your docs are not at the root of the repo:
+    docsDir: 'docs',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'master',
+    // defaults to false, set to true to enable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'Help us improve this page!'
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,24 @@
+---
+home: true
+heroImage: 
+actionText: Get Started →
+actionLink: /Guide.html
+features:
+- title: Simple
+  details: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- title: Effective
+  details: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- title: Lightweight
+  details: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+footer: MIT Licensed
+---
+
+### Installation
+
+```
+npm install --save tosin
+```
+
+### Usage
+
+
